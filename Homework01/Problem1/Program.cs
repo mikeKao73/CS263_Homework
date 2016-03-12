632687177412a946a3076f8785246c9a0267ce57
mikeKao73 <ksm731203@gmail.com>
2016-03-12 16:40:41 +0800

    Homework1

---
+++ Homework01/Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte number0 = 254;
+            byte x = 1;
+            byte number1 = (byte)(number0 + x);
+            byte number2 = (byte)(number1 + x);
+            //int number2 = number0 + 1;
+
+            Console.WriteLine(number0);
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+        }
+    }
+}
---
+++ Homework01/Problem2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student someone = new Student()
+            {
+                name = "mikeKao",
+                information = new Contact()
+                {
+                    address = "台北市羅斯福路123號",
+                    phone = "0938938938",
+                    email = "gmail@gmail.com",
+                    birthday = "1984-12-03",
+                    bloodType = "A"
+                }
+            };
+
+            Console.WriteLine(someone.name);
+            Console.WriteLine(someone.information.address);
+            Console.WriteLine(someone.information.phone);
+            Console.WriteLine(someone.information.email);
+            Console.WriteLine(someone.information.birthday);
+            Console.WriteLine(someone.information.bloodType);
+        }
+    }
+}
