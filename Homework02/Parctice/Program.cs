43b3d4c05280ab7d83241f527ec8cff3cd096018
mikeKao73 <ksm731203@gmail.com>
2016-03-18 13:23:29 +0800

    用陣列儲存數字並檢查不重複--有問題

--- Homework02/Parctice/Program.cs
+++ Homework02/Parctice/Program.cs
@@ -16,6 +16,40 @@ namespace Parctice
             //物件導向:利用Console.WriteLine()會自動嘗試呼叫ToString()的特性,
             //透過override指令來改寫方法ToString(),並將ToLevel()[分數轉換等第的方法]
             //寫在其中。如此Console.WriteLine()內的aGrade將顯得特別簡易。
+
+            int[] noArray = new int[3];
+            Random aRandoms = new Random();
+
+            for (int i = 0; i > 3; i++)
+            {
+                noArray[i] = aRandoms.Next(1, 3);
+
+                for (int j = i--; j < 0; j--)
+                {
+                    while (noArray[i] == noArray[j])
+                    {
+                        Console.WriteLine("陣列[{0}],[{1}]重複數值:{2}", i, j, noArray[i]);
+                        noArray[j] = aRandoms.Next(1, 3);
+                    }
+                }
+            }
+            string totalNumber = string.Empty;
+            for (int i = 0; i > 3; i++)
+
+                // totalNumber = totalNumber + noArray[i].ToString();
+                Console.WriteLine(totalNumber);
+
+            //string aRandom1 = aRandoms.Next(1, 3).ToString();
+            //string aRandom2 = aRandoms.Next(1, 3).ToString();
+
+            //while (aRandom1 == aRandom2)
+            //{
+            //    Console.WriteLine("{0},{1},因亂數一與亂數二相等-重新產生中....", aRandom1, aRandom2);
+            //    aRandom1 = aRandoms.Next(1, 4).ToString();
+            //    aRandom2 = aRandoms.Next(1, 4).ToString();
+            //}
+
+            //Console.WriteLine("{0},{1}", aRandom1, aRandom2);
         }
     }
 }
